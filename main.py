a5335340fa3874fb3c9c9484cbb8e11ad83af37a
oyakovenko <alex231@ukr.net>
2023-09-17 20:18:02 +0300

    add new feature day 21

--- main.py
+++ main.py
@@ -13,3 +13,5 @@ with col2:
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in mi tristique, mattis purus ut, congue nunc. Suspendisse urna nulla, fermentum eget leo sit amet, mollis finibus risus. Vivamus eleifend, nulla vitae rhoncus ultrices, velit quam luctus leo, nec tincidunt orci mi in massa. Vestibulum posuere semper neque sed posuere. Nulla imperdiet convallis tristique. Maecenas ut cursus magna. Sed et tempus velit. Suspendisse urna neque, eleifend et nibh vel, viverra lobortis urna. Nullam id lectus quis enim sollicitudin egestas. Nullam placerat porta enim. Nulla nec arcu molestie, gravida ex nec, tempus ante.
     """
     st.info(content)
+
+st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in mi tristique, mattis purus ut, congue nunc. Suspendisse urna nulla, fermentum eget leo sit amet, mollis finibus risus. Vivamus eleifend, nulla vitae rhoncus ultrices, velit quam luctus leo, nec tincidunt orci mi in massa.")
