f50a45ced8c714d0e5f09ff1720957e63607d712
oyakovenko <alex231@ukr.net>
2023-09-19 17:38:57 +0300

    add description, images, links day22

--- main.py
+++ main.py
@@ -30,14 +30,19 @@ content2 = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
 st.write(content2)
 
 
-col3, col4 = st.columns(2)
+col3, empty_cod, col4 = st.columns([1.5, 0.5, 1.5])
 df = pandas.read_csv("data.csv", ";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row['title'])
-
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[source code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[source code]({row['url']})")
